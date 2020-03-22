71932d1c0fec44d3d4a0bd52871a1f8c4502035c
denvercoder2 <riverholley0923@gmail.com>
2020-03-22 14:36:52 -0500

    Committed changes

--- RoundStart.lua
+++ RoundStart.lua
@@ -4,8 +4,16 @@ local IntermissionTime = 10
 local RoundTime = 10
 local waitTime = .50
 local roundCounter = 0
+
+local playerHealth = 100
+local killer_damage = 25
+
 local diffeq = math.random(1, 10)
 local map = game.Workspace.Crossroads
+local Players = game:GetService("Players"):GetPlayers()
+
+local survive = game.Teams.Survivor
+local killer = game.Teams.Killer
 
 -- grabbing the spawn position
 local Spawn_x = game.Workspace.Lobby.SpawnLocation1.Position.X
@@ -17,9 +25,6 @@ local TPort1 = {map.Teleport1.Position.X, map.Teleport1.Position.Y, map.Teleport
 local TPort2 = {map.Teleport2.Position.X, map.Teleport2.Position.Y, map.Teleport2.Position.Z}
 local KPort  = {map.KILLERSPOT.Position.X, map.KILLERSPOT.Position.Y, map.KILLERSPOT.Position.Z}
 
--- Local teams
-local survivers
-local killer
 
 -- Local Functions
 local function initialize()
@@ -31,7 +36,6 @@ end
 local function pickKiller()
 	wait(10)
 	-- This function should randomly pick a killer
-	local Players = game:GetService("Players"):GetPlayers()
 	local randomPlayer
 	if #Players > 0 then
 	    randomPlayer = Players[math.random(#Players)]
@@ -40,7 +44,6 @@ local function pickKiller()
 	
 	return killer
 end
-
 
 local function intermission()
 	-- countdown and wait for intermission time
@@ -70,10 +73,12 @@ while true do
 		print("WELCOME TO THE LOBBY . . .")
 		print("Killer will be picked soon . . .")
 		intermission()
-		
 	end
+
+
 	killer = pickKiller()
 	print("The Killer for the round is ----> ", killer)	
+
 	TeleportPlayer(TPort1[1], TPort1[2], TPort1[3])	
 	print("Begin Round")
 	initialize()
