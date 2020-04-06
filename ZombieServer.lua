55f6c6532f6d92f4894672b9cbac14269486fc30
denvercoder2 <riverholley0923@gmail.com>
2020-04-05 21:24:28 -0500

    Updated zombie server script

--- ZombieServer.lua
+++ ZombieServer.lua
@@ -1,6 +1,7 @@
 local lobby = game.Workspace.Test_Caben1
 local playerSpawn = game.Workspace.PLAYERSPAWN.Position
-
+local counter
+local algorithm
 local TPort1 = {playerSpawn.X, playerSpawn.Y, playerSpawn.Z}
 -- FUNCTIONS
 
@@ -19,17 +20,36 @@ local sounds = game.Workspace.Sound
 local audioID = sounds.SoundId
 local music = Instance.new("Sound", game.Workspace)
 local maxPlayer = 8
-
-
 local enemy = game.ServerStorage["Drooling Zombie"]
 local spawn = game.Workspace.SpawnDoor1.SPAWNDOOR
-local multiplier = 2
-local zombieCounter = 10
 	
+-------------------------------------------------------------------------------------------------------------------
 local function playMusic(count)
 	local id = Sounds[count]
 	music.SoundId = id
 	music:Play()
+end
+
+local function onPlayerAdded(player)
+	print("Player: " .. player.Name)
+end
+
+
+-- spawn boss zombies function here
+
+local function ZombieSpawnAlgorithm()
+	counter = 0
+	local Players = game:GetService("Players")
+
+	for _, player in pairs(Players:GetPlayers()) do
+		counter = counter + 1
+		onPlayerAdded(player)
+	end
+	Players.PlayerAdded:Connect(onPlayerAdded)
+	algorithm = 2 * counter + math.random(1, 10)
+	print("Zombies this round: ", algorithm)
+	
+	return algorithm
 end
 
 
@@ -37,9 +57,7 @@ end
 local function TeleportPlayer(X,Y,Z)
 	local target = CFrame.new(X,Y,Z) 
 	for i, player in ipairs(game.Players:GetChildren()) do
-   --Make sure the character exists and its HumanoidRootPart exists
    		if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-       --add an offset of 5 for each character
      	 	player.Character.HumanoidRootPart.CFrame = target + Vector3.new(0, i * 5, 0)
    		end
 	end
@@ -55,13 +73,27 @@ local function spawnZombies(counter)
 	end
 end
 
+local function SpawnGun()
+	local replicatedstorage = game:GetService("ReplicatedStorage")
+	local tool = replicatedstorage:WaitForChild("AR")
+	local cangive = true
+	game.Players.PlayerAdded:Connect(function(player)
+	    if cangive then
+	        tool:Clone().Parent = player:WaitForChild("Backpack")
+	        tool:Clone().Parent = player:WaitForChild("StarterGear") -- they spawn with it
+	    end
+	end)
+	
+end
 
-wait(10)
-TeleportPlayer(TPort1[1], TPort1[2], TPort1[3])
+local function RoundStart()
+	wait(10)
+	TeleportPlayer(TPort1[1], TPort1[2], TPort1[3])
+	local count = math.random(1, table.getn(Sounds))
+	playMusic(count)
+	counter = ZombieSpawnAlgorithm()
+	spawnZombies(counter)
+end
 
-local count = math.random(1, table.getn(Sounds))
-local counter = 10
-playMusic(count)
-
-
-spawnZombies(counter)
+SpawnGun()
+RoundStart()
